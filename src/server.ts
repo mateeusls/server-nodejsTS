65854f4a7ecce2560ccfe8520782d44fa6c8ce16
mateeusls <magolopes20@gmail.com>
2023-01-30 06:15:31 -0300

    First commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express"
+import { router } from "./Routes"
+const app = express()
+
+app.use(express.json())
+
+app.use(router)
+
+app.listen(3333, () => console.log("Server is running in the port 3333"))
