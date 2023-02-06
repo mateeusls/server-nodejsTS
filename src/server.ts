0fde23d0ec100cc612d116b7cde961c93a8d67e8
mateeusls <magolopes20@gmail.com>
2023-02-06 20:53:24 -0300

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,18 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+	);
+	//Quais são os métodos que a conexão pode realizar na API
+	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	app.use(cors());
+	next();
+});
 app.use(router);
 
 app.listen(3333, () => console.log("Server is running in the port 3333"));
