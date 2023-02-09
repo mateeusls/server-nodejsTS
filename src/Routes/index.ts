151315c4c65b9c70a2dae8bfd09e192b6c330768
mateeusls <magolopes20@gmail.com>
2023-02-09 05:58:06 -0300

    Change delete integrations sesuite

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -5,13 +5,11 @@ export const router = Router();
 import { authRouter } from "./auth";
 import { confeaApiRouter } from "./confeaApi";
 import { userRouter } from "./user";
-import { workflowRouter } from "./workflow";
 
 router.get("/", verifyToken, (req: Request, res: Response) => {
 	return res.json(req.cookies.userId);
 });
 
 router.use("/user", userRouter);
-router.use("/wf", workflowRouter);
 router.use("/api", confeaApiRouter);
 router.use("/auth", authRouter);
